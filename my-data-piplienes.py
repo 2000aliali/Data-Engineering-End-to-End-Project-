e5c7568587a99c4396bd4edf0065dbad10608624
2000aliali <126164711+2000aliali@users.noreply.github.com>
2023-10-29 01:46:29 +0100

    Update my-data-piplienes.py

--- my-data-piplienes.py
+++ my-data-piplienes.py
@@ -3,6 +3,7 @@ import sys
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+
 # Determine the path to the parent directory
 parent_folder = os.path.dirname(os.path.abspath(__file__))
 scripts_folder = os.path.join(parent_folder, 'dags')
@@ -10,12 +11,10 @@ scripts_folder = os.path.join(parent_folder, 'dags')
 # Add the 'python_scripts' folder to the system path
 if scripts_folder not in sys.path:
     sys.path.append(scripts_folder)
-#parent_folder = os.path.dirname(os.path.abspath(__file__))
-#scripts_folder = os.path.join(parent_folder, 'python_scripts')
 
 from python_scripts.write_csv_to_postgres import write_csv_to_postgres_main
 from python_scripts.write_df_to_postgres import write_df_to_postgres_main
-# ... rest of your DAG code ...
+# rest of your DAG code 
 start_date = datetime(2023, 10, 28, 18, 34)
 
 default_args = {
@@ -27,10 +26,6 @@ default_args = {
 
 with DAG('csv_extract_airflow_docker', default_args=default_args, schedule='@daily') as dag:
 
-
-
-
-    
 
     write_csv_to_postgres = PythonOperator(
         task_id='write_csv_to_postgres',
